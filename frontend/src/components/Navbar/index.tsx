ff91f04caa2525333fad069a5cb3caad5f59bda4
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-10 10:07:10 -0300

    NavBar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GithubIcon } from 'assets/img/GitHub.svg';
+import './styles.css'
+
+function Navbar() {
+    
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DsMovie</h1>
+                    <a href="https://github.com/LuccasLpn">
+                        <div className='dsmovie-contact-container'>
+                            <GithubIcon />
+                            <p className='dsmovie-contact-link'>/LuccasLpn</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+
+}
+export default Navbar;
