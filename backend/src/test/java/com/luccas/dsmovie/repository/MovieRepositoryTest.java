d8d790b8fb02e8320fab40942464b3d22b847696
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-23 13:23:54 -0300

    Test Update and Delete

--- backend/src/test/java/com/luccas/dsmovie/repository/MovieRepositoryTest.java
+++ backend/src/test/java/com/luccas/dsmovie/repository/MovieRepositoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
 @DataJpaTest
 @DisplayName("Test For MovieRepositorys")
@@ -27,6 +27,28 @@ class MovieRepositoryTest {
         Assertions.assertThat(savedMovie.getId()).isNotNull();
         Assertions.assertThat(savedMovie.getTitle()).isEqualTo(savedMovie.getTitle());
         Assertions.assertThat(savedMovie.getCount()).isEqualTo(savedMovie.getCount());
+    }
+
+    @Test
+    @DisplayName("Saved updates movie when SuccessFull")
+    public void save_UpdateMovie_WhenSuccessFull(){
+        Movie movie = MovieCreator.createdMovieToBeSaved();
+        Movie savedMovie = movieRepository.save(movie);
+        savedMovie.setTitle("Homem formiga");
+        Movie updateMovie = movieRepository.save(movie);
+        Assertions.assertThat(updateMovie).isNotNull();
+        Assertions.assertThat(updateMovie.getId()).isNotNull();
+        Assertions.assertThat(updateMovie.getTitle()).isEqualTo(updateMovie.getTitle());
+    }
+
+    @Test
+    @DisplayName("Delete removes movie when SuccessFull")
+    public void delete_RemovesMovie_WhenSuccessFull(){
+        Movie movie = MovieCreator.createdMovieToBeSaved();
+        Movie savedMovie = movieRepository.save(movie);
+        this.movieRepository.delete(savedMovie);
+        Optional<Movie> movieOptional = this.movieRepository.findById(savedMovie.getId());
+        Assertions.assertThat(movieOptional.isEmpty());
     }
 
 }
